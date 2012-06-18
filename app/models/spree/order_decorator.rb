da5588b7477ba587fee82915e5910f2924949e3e
Abhijit Sinha <abhijit.sinha@kiprosh.com>
2012-06-18 21:26:44 +0530

    index method overridden for order and taxonomy

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,4 +1,12 @@
 Spree::Order.class_eval do
   belongs_to :store
   scope :by_store, lambda { |store| where(:store_id => store.id) }
+
+  before_filter :order_store, :only => :index
+
+  def order_store
+    @order = Spree::Order.by_store(current_store.id) if current_store.id
+  end
+
 end
+
--- app/models/spree/taxonomy_decorator.rb
+++ app/models/spree/taxonomy_decorator.rb
@@ -1,3 +1,12 @@
 Spree::Taxonomy.class_eval do
   belongs_to :store
+  scope :by_store, lambda { |store| where(:store_id => store.id) }
+
+  before_filter :taxonomy_store, :only => :index
+
+  def order_store
+    @taxonomy = Spree::Taxonomy.by_store(current_store.id) if current_store.id
+  end
+
 end
+
