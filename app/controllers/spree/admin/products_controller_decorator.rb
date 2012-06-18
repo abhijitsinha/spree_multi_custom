b8455f3e7f8d6c856d6f7629f936895f7b7a45d4
Abhijit Sinha <abhijit.sinha@kiprosh.com>
2012-06-18 19:29:03 +0530

    products for the store

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -1,5 +1,10 @@
 Spree::Admin::ProductsController.class_eval do
   update.before :set_stores
+  before_filter :product_store, :only => :index
+
+  def product_store
+    Spree:Product.by_store(current_store_id) if current_store_id
+  end
 
   private
   def set_stores
@@ -7,3 +12,4 @@ Spree::Admin::ProductsController.class_eval do
   end
 
 end
+
