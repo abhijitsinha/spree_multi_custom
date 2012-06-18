d8b7936f44cee78650fe57b26d498f05be7afe30
Abhijit Sinha <abhijit.sinha@kiprosh.com>
2012-06-18 20:17:44 +0530

    products for the store

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -6,6 +6,12 @@ Spree::Admin::ProductsController.class_eval do
     @collection = Spree::Product.by_store(current_store.id) if current_store.id
   end
 
+  def create
+    super
+    @product.store_ids = current_store.id
+    @product.save
+  end
+
   private
   def set_stores
     @product.store_ids = nil unless params[:product].key? :store_ids
